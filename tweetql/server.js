72c79d3d4336d720ab5d522343de3a7731fc088c
fluorjo <fluorjo@naver.com>
2023-10-31 05:02:02 +0900

    4.8 Type Resolvers

--- tweetql/server.js
+++ tweetql/server.js
@@ -11,12 +11,26 @@ let tweets = [
   },
 ];
 
+let users = [
+  {
+    id: "1",
+    firstName: "a",
+    lastName: "b",
+  },
+  {
+    id: "2",
+    firstName: "jjj",
+    lastName: "fff",
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
-    username: String!
     firstName: String!
-    lastName: String
+    lastName: String!
+    fullName: String!
+
   }
   type Tweet {
     id: ID!
@@ -24,6 +38,7 @@ const typeDefs = gql`
     author: User
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
   }
@@ -40,6 +55,9 @@ const resolvers = {
     },
     tweet(root, { id }) {
       return tweets.find((tweet) => tweet.id === id);
+    },
+    allUsers() {
+      return users;
     },
   },
   Mutation: {
@@ -60,6 +78,11 @@ const resolvers = {
       return true;
     },
   },
+  User:{
+    fullName({firstName, lastName}){
+      return `${firstName}${lastName}`
+    }
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
