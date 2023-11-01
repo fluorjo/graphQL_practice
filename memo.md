86d08dc836ed6b9f3e22e193d8c9b638a4d8759d
fluorjo <fluorjo@naver.com>
2023-11-02 00:48:20 +0900

    #1.1 ApolloProvider

--- memo.md
+++ memo.md
@@ -1,3 +1,5 @@
 [graphiQL](https://studio.apollographql.com/public/star-wars-swapi/variant/current/explorer
 
 )
+- 1.1
+  - 프로바이더란? 애플리케이션 내부에 있는 것들이 client에 접근할 수 있게 해주는 것. 
--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache,  } from "@apollo/client";
 
 //1.아폴로 클라이언트 생성
 const client = new ApolloClient({
@@ -6,18 +6,5 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-//2.이 쿼리를 실행 중인 localhost:4000으로 전송
-client
-.query({
-    query: gql`
-      {
-        allMovies {
-          title
-        }
-      }
-    `,
-  })
-//3. 데이터 도착 후 콘솔에 출력.
-  .then((data) => console.log(data));
 
 export default client;
